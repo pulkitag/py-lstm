d774ac7f99e524a85c26ab9de2ce29dd5a235a47
pulkitag <pulkitag@berkeley.edu>
2015-06-22 13:00:16 -0700

    ReLU layer in progress

--- layers.py
+++ layers.py
@@ -3,34 +3,76 @@
 
 import numpy as np
 import scipy.misc as scm
+import copy
 
 ##
 # The base class from which other layers will inherit. 
 class BaseLayer:
-	def __init__(self, prms):
+	def __init__(self, prms=None):
 		#Type of layers
 		self.type_ = None
 		#Name of the layers
 		self.name_ = None
 		#The layer parameters - these can
 		#be different for different layers
-		self.prms_ = None
+		self.prms_ = prms
+		#The output of the layer
+		self.top_  = None
+		#The gradients wrt to the parameters and the bottom
+		self.grad_ = {} 
 
-	def forward(self, bottom, top):
+	#Forward pass
+	def forward(self, bottom):
 		pass
 
-	def backward(self, bottom, top):
+	#Backward pass
+	def backward(self, bottom, topgrad):
+		'''
+			bottom: The inputs from the previous layer
+			top   : The gradient from the next layer
+		'''
 		pass
 
-	def setup(self, bottom, top):
+	#Setup the layer including the top
+	def setup(self, bottom):
 		pass
 
+	def get_mutable_top(self):
+		return self.top_
 
+	def get_top(self):
+		return copy.deepcopy(self.top_)
+
+	def get_top_shape(self):
+		return self.top_.shape
+
+	def get_gradient(self, gradType='bot'):
+		'''
+			gradType: bot - gradients with respect to the bottom inputs
+								in general it can be the name of parameters wrt to which
+								gradient is required
+		'''
+		assert gradType in self.grad_.keys(), 'gradType: %s is not recognized' % gradType
+		return copy.deepcopy(self.grad_[gradType])	
+
+	def get_mutable_gradient(self, gradType):
+		'''
+			See get_gradient for the docs
+		'''	
+		assert gradType in self.grad_.keys(), 'gradType: %s is not recognized' % gradType
+		return copy.deepcopy(self.grad_[gradType])	
+
+##
+# Recitified Linear Unit (ReLU)
 class ReLU(BaseLayer):
-	def setup(self, bottom, top):
-		top = np.zeros_like(bottom)
+	def setup(self, bottom):
+		self.top_         = np.zeros_like(bottom)
+		self.grad_['bot'] = np.zeros_like(bottom) 
 
-	def forward(self, bottom, top):
-		top = np.maximum(bottom, 0)
+	def forward(self, bottom):
+		self.top_ = np.maximum(bottom, 0)
+
+	def backward(self, bottom, topgrad):
+		
 	
 	
--- test.py
+++ test.py
@@ -4,3 +4,9 @@
 
 import numpy as np
 import layers as ly
+
+def test_relu():
+	relu   = ly.ReLU()
+	bottom = np.random.random((4,5))
+	top    = np.zeros_like(bottom)
+	 	
