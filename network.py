ae26a39a3b2b67825f7e94d52669a2203856178c
philkr <philkr@users.noreply.github.com>
2015-06-22 18:41:53 -0700

    Network cleaned up, parameters and gradients can now be returned

--- network.py
+++ network.py
@@ -3,16 +3,16 @@ import numpy as np
 class Network:
 	def __init__(self):
 		# Stores layers in the format (layer_object,input_names,output_names)
-		self.layers = []
-		self.blobs = {}
-		self.diffs = {}
-		self.blob_history = []
+		self._layers = []
+		self._blobs = {}
+		self._diffs = {}
+		self._blob_history = []
 	
 	def _get_blobs(self,names):
-		return [self.blobs[n] for n in names]
+		return [self._blobs[n] for n in names]
 	
 	def _get_diffs(self,names):
-		return [self.diffs[n] for n in names]
+		return [self._diffs[n] for n in names]
 	
 	def addLayer(self,layer_name,layer_instance,input_blobs,output_blobs):
 		"""
@@ -20,27 +20,32 @@ class Network:
 		"""
 		if not isinstance(input_blobs ,list): input_blobs  = [input_blobs]
 		if not isinstance(output_blobs,list): output_blobs = [output_blobs]
-		self.layers.append( (layer_name,layer_instance,input_blobs,output_blobs) )
+		self._layers.append( (layer_name,layer_instance,input_blobs,output_blobs) )
 	
 	def setup(self,**kwargs):
 		"""
 		    Setup the network
 		"""
-		for n,l,i,o in self.layers:
+		for n in kwargs:
+			self._blobs[n] = 0*kwargs[n]
+		
+		for n,l,i,o in self._layers:
 			for s in i+o:
-				if not s in self.blobs:
-					self.blobs[s] = np.empty((0,))
+				if not s in self._blobs:
+					self._blobs[s] = np.empty((0,))
 			l.setup( self._get_blobs(i), self._get_blobs(o) )
+		for n in self._blobs:
+			self._diffs[n] = 0*self._blobs[n]
 	
 	def forward1(self,**kwargs):
 		"""
 		    Run a single forward pass (not recurrently)
 		    Initialize the blob values using the keyword arguments
 		"""
-		for n,v in kwargs:
-			self.blobs[n][...] = v
+		for n in kwargs:
+			self._blobs[n][...] = kwargs[n]
 		
-		for n,l,i,o in self.layers:
+		for n,l,i,o in self._layers:
 			l.forward( self._get_blobs(i), self._get_blobs(o) )
 	
 	def backward1(self,**kwargs):
@@ -48,21 +53,30 @@ class Network:
 		    Run a single backward pass (not recurrently)
 		    Initialize the diff values using the keyword arguments
 		"""
-		for n,v in kwargs:
-			self.diffs[n][...] = v
+		for n in kwargs:
+			self._diffs[n][...] = kwargs[n]
 		
-		for n,l,i,o in self.layers[::-1]:
+		for n,l,i,o in self._layers[::-1]:
 			l.backward( self._get_blobs(i), self._get_blobs(o), self._get_diffs(i), self._get_diffs(o) )
 		
 	#def forwardBackward(self,**kwargs):
 		#pass
+	
+	@property
+	def blobs(self):
+		return self._blobs
+	
+	@property
+	def diffs(self):
+		return self._diffs
+	
 	@property
 	def gradient(self):
 		"""
 		    Return the gradient of all layers
 		"""
 		r = {}
-		for n,l,i,o in self.layers:
+		for n,l,i,o in self._layers:
 			if not n in r:
 				r[n] = l.gradient
 			else:
@@ -79,7 +93,7 @@ class Network:
 		    Return the gradient of all layers
 		"""
 		r = {}
-		for n,l,i,o in self.layers:
+		for n,l,i,o in self._layers:
 			if not n in r:
 				r[n] = l.flat_gradient
 			else:
@@ -88,26 +102,47 @@ class Network:
 				assert set(g.keys()) == set(m.keys())
 				for m in g:
 					r[n][m] += g[m]
-		return np.concatenate( r.values(), axis=0 )
+		return np.concatenate( [r[n] for n in sorted(r.keys())], axis=0 )
 	
 	def pushState(self):
 		"""
 		    Push the current blobs on a stack (so that we can remember them later)
 		"""
 		import copy
-		self.blob_history.append( copy.deepcopy(self.blobs) )
+		self._blob_history.append( copy.deepcopy(self._blobs) )
 	
 	def popState(self):
-		self.blobs = self.blob_history.pop()
+		self._blobs = self._blob_history.pop()
 	
 	@property
 	def parameters(self):
-		""" TODO: Implement """
-		pass
+		""" Return the parameters of the model """
+		r = {}
+		for n,l,i,o in self._layers:
+			if not n in r:
+				r[n] = l.parameters
+			else:
+				# Add the parmaters
+				g = l.parameters
+				assert set(g.keys()) == set(m.keys())
+				for m in g:
+					r[n][m] += g[m]
+		return r
+	
 	@property
-	def flat_parameters(self, value):
-		""" TODO: Implement """
-		pass
+	def flat_parameters(self):
+		""" Return the flat parameters of the model """
+		r = {}
+		for n,l,i,o in self._layers:
+			if not n in r:
+				r[n] = l.flat_parameters
+			else:
+				# Add the parmaters
+				g = l.flat_parameters
+				assert set(g.keys()) == set(m.keys())
+				for m in g:
+					r[n][m] += g[m]
+		return np.concatenate( [r[n] for n in sorted(r.keys())], axis=0 )
 	@flat_parameters.setter
 	def flat_parameters(self, value):
 		""" TODO: Implement """
--- test_network.py
+++ test_network.py
@@ -3,5 +3,18 @@ from layers import *
 
 l = network.Network()
 l.addLayer( 'ReLU1', ReLU(), 'data', 'positive_data' )
+l.addLayer( 'InnerProduct1', InnerProduct(output_shape=10), 'data', 'score' )
+l.addLayer( 'Sigm', Sigmoid(), 'score', 'out' )
 
-l.setup( data=np.ones((10,10)) )
+l.setup( data=np.ones((5,10)) )
+print( [(n,v.shape) for n,v in l.blobs.items()] )
+
+l.forward1( data=np.random.normal(0,10,(5,10)).astype(int) )
+l.backward1( out=np.random.random(l.blobs['out'].shape)>0.5 )
+#print( l.blobs['data'] )
+#print( l.blobs['positive_data'] )
+#print( l.blobs['out'] )
+#print( l.diffs['data'] )
+print( l.parameters )
+print( l.flat_parameters )
+
