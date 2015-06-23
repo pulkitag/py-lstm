54120feded03542ce979116ef4d600d7f4afd539
Tinghui Zhou <tinghuiz@knish.EECS.Berkeley.EDU>
2015-06-23 15:20:21 -0700

    SGD for training

--- solver.py
+++ solver.py
@@ -3,41 +3,55 @@
 
 import numpy as np
 import random
+import scipy.optimize as sopt
 
 class Solver:
-	def __init__(self):
-		
-	#Train net
+    def __init__(self):
+        # self.net_ = net
+        pass
+
+	#Train net (SGD)
 	def train(self, net, data, labels, params):
-		net.init_weights()
+        # data: list of NxD arrays. N is the sequence length (varies 
+        #    for different instances)
+        # labels: list of target output sequences
+        
+        if params.train_scratch:
+		  net.init_random()
         N = len(data)
         tmp = list(zip(data, labels))
         random.shuffle(tmp)
         data, labels = zip(*tmp)
+        # TODO: mini-batches?
         for i in xrange(0, N):
         	data_seq = data[i]
         	label_seq = labels[i]
-        	# Pass to network backward func
+            # Pass to network backward function
+            net.forwardBackwardAll()
+            # Compute loss
+            # net.compute_loss(label_seq)
+            # Backward pass
+            # net.backward_all()
+        	# Update weights
+            grads = params.momentum * prev_grads 
+                + params.learn_rate * curr_grads
+            net.flat_parameters += grads
+            # Snapshot the net
+            if i % params.snapshot == 0:
+                pass
+            # Update learning rate
+            if i % params.stepsize == 0:
+                params.learn_rate *= params.gamma
         return net
 
     #Test net
-    def test(self, net, data, params):
-    	N = len(data)
-    	pred = []
-    	for i in xrange(0, N):
-    		data_seq = data[i]
-    		net.reset()
-    		out = net.forward_all(input=data_seq)
-    		pred.append(out)
-    	return pred
-
-    #Gradient checking for a layer
-    def check_grad_layer(self, layer, data):
-    	top = np.zeros_like(lay.top_)
-    	# FIXME: pass by reference in python?
-    	layer.forward(bottom, top)
-    	#The following check_grad() takes a function as input
-    	f = lambda x : x
-    	err = scipy.optimize.check_grad(f(top), 
-    		layer.get_gradient(), data)
-    	return err
+   def test(self, net, data, params):
+        N = len(data)
+        pred = []
+        for i in xrange(0, N):
+   		   data_seq = data[i]
+           # Reset the net for new sequence input
+   		   net.reset()
+   		   out = net.forward_all(input=data_seq)
+   		   pred.append(out)
+        return pred
